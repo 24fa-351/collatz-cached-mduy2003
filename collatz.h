a2ec0cc47bcfe3a7466de2d51abc03cbb6778dd6
mduy2003 <mduy2003@csu.fullerton.edu>
2024-09-27 23:10:28 -0700

    Before caches

---
+++ collatz.h
@@ -0,0 +1,24 @@
+unsigned long long int collatz_conjecture(unsigned long long int value)
+{
+    
+        if (value % 2 == 0)
+        {
+            value /= 2;
+        }
+        else
+        {   
+            value = 3 * value + 1;
+        }
+    return value;
+}
+
+unsigned long long int collatz_conjecture_wrapper(unsigned long long int value)
+{
+    unsigned long long int steps = 0;
+    while (value != 1 && value > 0)
+    {
+        steps++;
+        value = collatz_conjecture(value);
+    }
+    return steps;
+}
---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "collatz.h"
+
+int main(int argc, char *argv[])
+{
+    unsigned long long int num_of_values;
+    unsigned long long int min;
+    unsigned long long int max;
+    char* cache_policy;
+    int cache_size;
+    FILE *file = fopen("results.csv", "w+");
+
+    sscanf(argv[1], "%llu", &num_of_values);
+    sscanf(argv[2], "%llu", &min);
+    sscanf(argv[3], "%llu", &max);
+    sscanf(argv[4], "%s", cache_policy);
+    sscanf(argv[5], "%d", &cache_size);
+
+    srand(time(NULL));
+
+    fprintf(file, "Number, Number of Steps\n");
+    for (size_t ix = 0; ix < num_of_values; ++ix)
+    {
+        unsigned long long int random_number = rand() % (max - min + 1) + min;
+        unsigned long long int num_of_steps = collatz_conjecture(random_number);
+        fprintf(file, "%llu, %llu\n", random_number, num_of_steps);
+    }
+    fclose(file);
+
+    return 0;
+}
