5ac83b8d800ede450076080d1544e786ad24ecf6
mduy2003 <mduy2003@csu.fullerton.edu>
2024-12-16 00:47:56 -0800

    Reformatted

--- cache.h
+++ cache.h
@@ -7,13 +7,13 @@ typedef struct {
 } CacheEntry;
 
 typedef struct {
-    CacheEntry *entries;
+    CacheEntry* entries;
     int size;
     int capacity;
 } LRUCache;
 
 typedef struct {
-    CacheEntry *entries;
+    CacheEntry* entries;
     int size;
     int capacity;
     int front;
@@ -24,11 +24,15 @@ LRUCache* create_lru_cache(int capacity);
 
 FIFOCache* create_fifo_cache(int capacity);
 
-unsigned long long int get_from_lru_cache(LRUCache *cache, unsigned long long int key, int *cache_hits, int *cache_accesses);
+unsigned long long int get_from_lru_cache(LRUCache* cache,
+    unsigned long long int key, int* cache_hits, int* cache_accesses);
 
-void put_in_lru_cache(LRUCache *cache, unsigned long long int key, unsigned long long int value);
+void put_in_lru_cache(
+    LRUCache* cache, unsigned long long int key, unsigned long long int value);
 
-unsigned long long int get_from_fifo_cache(FIFOCache *cache, unsigned long long int key, int *cache_hits, int *cache_accesses);
+unsigned long long int get_from_fifo_cache(FIFOCache* cache,
+    unsigned long long int key, int* cache_hits, int* cache_accesses);
 
-void put_in_fifo_cache(FIFOCache *cache, unsigned long long int key, unsigned long long int value);
+void put_in_fifo_cache(
+    FIFOCache* cache, unsigned long long int key, unsigned long long int value);
 #endif
